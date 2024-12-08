b1cc747a3114c2e77125fd2e1b072fe83a01cc18
franv <franveramoran@gmail.com>
2024-12-07 21:57:25 -0500

    update arreglo de errores

--- app/src/main/java/com/example/miapp2/MainActivity.java
+++ app/src/main/java/com/example/miapp2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.miapp2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,10 +10,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.airbnb.lottie.LottieAnimationView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
@@ -21,4 +26,10 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+    public void btn_iniciar (View view){
+        Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+        startActivity(intent);
+    }
+
+
 }
